2e2e85986aa8e37d8e4468e82b79bbc918237c2e
MichaeGon <lodnix@gmail.com>
2016-07-25 16:51:14 +0900

    skl

--- src/Setup.hs
+++ src/Setup.hs
@@ -1,4 +1,22 @@
+import Distribution.PackageDescription
+import Distribution.Simple
+import Distribution.Simple.LocalBuildInfo
+import Distribution.Simple.Setup
 
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main = defaultMainWithHooks simpleUserHooks { preBuild = myPreBuildHook
+                                            , postBuild = myPostBuildHook }
+
+myPreBuildHook :: Args -> BuildFlags -> IO HookedBuildInfo
+myPreBuildHook arg bf = undefined
+                    >> simplePreBuild arg bf
+
+myPostBuildHook :: Args -> BuildFlags -> PackageDescription -> LocalBuildInfo -> IO ()
+myPostBuildHook arg bf pd lbi = simplePostBuild arg bf pd lbi
+                            >> undefined
+
+simplePreBuild ::  Args -> BuildFlags -> IO HookedBuildInfo
+simplePreBuild = preBuild simpleUserHooks
+
+simplePostBuild :: Args -> BuildFlags -> PackageDescription -> LocalBuildInfo -> IO ()
+simplePostBuild = postBuild simpleUserHooks
