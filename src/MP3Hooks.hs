ebabee65f9cdcd4a6dae4333b161e88f61e65b3e
MichaeGon <lodnix@gmail.com>
2016-07-25 17:46:17 +0900

    witout TH

--- src/MP3Hooks.hs
+++ src/MP3Hooks.hs
@@ -1,16 +1,53 @@
-{-# LANGUAGE TemplateHaskell, QuasiQuotes #-}
 module MP3Hooks
     ( mp3PreBuildHook
     , mp3PostBuildHook
     ) where
 
+import Control.Monad
 import Data.List
-import Language.Haskell.TH
+import Data.Ord
+import System.Directory
+import System.Process
+import System.Random
 
-import MP3Hooks.Core
 
 mp3PreBuildHook :: IO ()
-mp3PreBuildHook = $(mp3PreBuildHookQ)
+mp3PreBuildHook = getCurrentDirectory
+            >>= getDirectoryContents
+            >>= shuffle . mp3s
+            >>= br
+            where
+                br [] = return ()
+                br xs = (void . runCommand . (\x -> "afplay \"" ++ x ++ "\" &") . head) xs
+
 
 mp3PostBuildHook :: IO ()
-mp3PostBuildHook = $(mp3PostBuildHookQ)
+mp3PostBuildHook = void (runCommand "killall afplay")
+
+{-}
+mp3PreBuildHookQ :: ExpQ
+mp3PreBuildHookQ = runIO ioActions >> [| return () |]
+    where
+        ioActions = getCurrentDirectory
+                >>= getDirectoryContents
+                >>= shuffle . mp3s
+                >>= br
+        br [] = return ()
+        br xs = (void . runCommand . (\x -> "afplay \"" ++ x ++ "\" &") . head) xs
+
+mp3PostBuildHookQ :: ExpQ
+mp3PostBuildHookQ = runIO ioActions >> [| return () |]
+    where
+        ioActions = void (runCommand "killall afplay")
+-}
+
+mp3s :: [FilePath] -> [FilePath]
+mp3s = filter (".mp3" `isSuffixOf`)
+
+shuffle :: [a] -> IO [a]
+shuffle xs = map fst . sortBy (comparing snd) <$> ys
+    where
+        ys = zip xs <$> (getRandoms :: IO [Int])
+
+getRandoms :: (Random a) => IO [a]
+getRandoms = randoms <$> getStdGen
