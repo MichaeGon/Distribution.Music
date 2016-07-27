cd8305c16cf378fba982198e4e435f9fc30a94c6
MichaeGon <lodnix@gmail.com>
2016-07-27 15:22:46 +0900

    build only

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,24 @@
+import Distribution.PackageDescription
 import Distribution.Simple
+import Distribution.Simple.LocalBuildInfo
+import Distribution.Simple.Setup
 
 import MP3Hooks
 
 main :: IO ()
-main = playMp3 >>= main'
-    where
-        main' (Just h) = defaultMain >> stopMp3 h
-        main' _ = defaultMain 
+main = defaultMainWithHooks simpleUserHooks { preBuild = myPreBuildHook
+                                            , postBuild = myPostBuildHook }
+
+myPreBuildHook :: Args -> BuildFlags -> IO HookedBuildInfo
+myPreBuildHook arg bf = playMp3
+                    >> simplePreBuild arg bf
+
+myPostBuildHook :: Args -> BuildFlags -> PackageDescription -> LocalBuildInfo -> IO ()
+myPostBuildHook arg bf pd lbi = simplePostBuild arg bf pd lbi
+                            >> stopMp3
+
+simplePreBuild ::  Args -> BuildFlags -> IO HookedBuildInfo
+simplePreBuild = preBuild simpleUserHooks
+
+simplePostBuild :: Args -> BuildFlags -> PackageDescription -> LocalBuildInfo -> IO ()
+simplePostBuild = postBuild simpleUserHooks
