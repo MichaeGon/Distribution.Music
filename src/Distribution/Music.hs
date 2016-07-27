cfc85b2a629d8e3b7bf2052fa3ba0db49ffa4b8e
MichaeGon <lodnix@gmail.com>
2016-07-27 16:16:32 +0900

    lib

---
+++ src/Distribution/Music.hs
@@ -0,0 +1,28 @@
+module Distribution.Music
+    ( musicMain
+    ) where
+
+import Distribution.PackageDescription
+import Distribution.Simple
+import Distribution.Simple.LocalBuildInfo
+import Distribution.Simple.Setup
+
+import MP3Hooks
+
+musicMain :: IO ()
+musicMain = defaultMainWithHooks simpleUserHooks { preBuild = myPreBuildHook
+                                            , postBuild = myPostBuildHook }
+
+myPreBuildHook :: Args -> BuildFlags -> IO HookedBuildInfo
+myPreBuildHook arg bf = playMp3
+                    >> simplePreBuild arg bf
+
+myPostBuildHook :: Args -> BuildFlags -> PackageDescription -> LocalBuildInfo -> IO ()
+myPostBuildHook arg bf pd lbi = simplePostBuild arg bf pd lbi
+                            >> stopMp3
+
+simplePreBuild ::  Args -> BuildFlags -> IO HookedBuildInfo
+simplePreBuild = preBuild simpleUserHooks
+
+simplePostBuild :: Args -> BuildFlags -> PackageDescription -> LocalBuildInfo -> IO ()
+simplePostBuild = postBuild simpleUserHooks
--- src/Main.hs
+++
@@ -1,9 +0,0 @@
-{-# LANGUAGE TemplateHaskell, QuasiQuotes #-}
-
--- dummy
-module Main where
-
-import Language.Haskell.TH
-import System.Process
-
-$( runIO (runCommand "pwd" >> runCommand "touch test.txt") >> [d| main = return () |] )
